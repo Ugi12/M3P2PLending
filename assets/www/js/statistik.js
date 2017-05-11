5e57a9d32d8f43c25c79582b090b6f597ec76292
Reshad Dernjani <a1409161@univie.ac.at>
2017-05-11 15:12:06 +0200

    Für das nav jetzt bitte einfach das nav.js einbinden, so bleibt das navbar immer gleich und muss nicht bei jeder seite aktualisiert werden Statistik update

--- assets/www/js/statistik.js
+++ assets/www/js/statistik.js
@@ -1,35 +1,20 @@
-$("#test").click(function(){
+/*$(document).ready(function(){
+    $("#test").click(function(){
         alert('statistik.js loaded!');
-        // Query the database
-        function queryDB(tx) {
-            tx.executeSql('SELECT * FROM USER', [], querySuccess, errorCB);
-        }
-
-        // Query the success callback
-        function querySuccess(tx, results) {
-            var len = results.rows.length;
-            alert("USER table: " + len + " row(s) found.");
-            for (var i=0; i<len; i++){
-                alert("Row = " + i + " ID = " + results.rows.item(i).id + " E-Mail =  " + results.rows.item(i).email);
-            }
-            // this will be true since it was a select statement and so rowsAffected was 0
-            if (!results.rowsAffected) {
-                alert('No rows affected!');
-                return false;
-            }
-            // for an insert statement, this property will return the ID of the last inserted row
-            alert("Last inserted row ID = " + results.insertId);
-        }
-
-        // Error callback function
-        function errorCB(err) {
-            alert("Error processing SQL: "+err.code);
-        }
-
-        // DB Funktion
-        //var db = localStorage.db;
-        var db = storage.getItem(database);
-        db.transaction(queryDB, errorCB, querySuccess);
-
+        MySql.Execute(
+            "sql3.freemysqlhosting.net",
+            "sql3173783",
+            "NDQRtTqcvt",
+            "sql3173783",
+            "select * from users",
+            function (data) {
+                alert('DB Success -> ' + data.Success);
+                var tmp = data.Result[0];
+                for (var key in tmp) {
+                  if (tmp.hasOwnProperty(key)) {
+                    alert(key + " -> " + tmp[key]);
+                  }
+                }
+        });
     });
-
+});*/
