46d787938745d9a3dbe535c3e35ea044107933dd
Reshad Dernjani <a1409161@univie.ac.at>
2017-05-11 12:34:05 +0200

    Sperated mysql query from msql connector

--- assets/www/js/mysql.js
+++ assets/www/js/mysql.js
@@ -15,24 +15,3 @@
         document.head.appendChild(sqlScript);
     }
 };
-
-//Testing Execute
-MySql.Execute(
-    "sql3.freemysqlhosting.net",
-    "sql3173783",
-    "NDQRtTqcvt",
-    "sql3173783",
-    "select * from users",
-    function (data) {
-        for (var key in data) {
-          if (data.hasOwnProperty(key)) {
-            alert(key + " -> " + data[key]);
-          }
-        }
-        var tmp = data.Result[0];
-        for (var key in tmp) {
-          if (tmp.hasOwnProperty(key)) {
-            alert(key + " -> " + tmp[key]);
-          }
-        }
-});
