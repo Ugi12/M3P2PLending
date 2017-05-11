b8437e81bdf9f20c11ba1b7fa48df7d0aa8d4a03
Reshad Dernjani <a1409161@univie.ac.at>
2017-05-11 22:43:34 +0200

    correction on database and help commets on mysql_test_query

--- assets/www/js/mysql.js
+++ assets/www/js/mysql.js
@@ -1,7 +1,4 @@
-﻿// Link to phpmyadmin zum testen von sql befehlen
-// https://u2831340.ct.sendgrid.net/wf/click?upn=eEpzx8Lj0S6zZ6zdOgTXWOVsbEfzkjSpaPCLOvQxGtA-3D_S3Ysil5CZbY-2BcU1OzhtQnANG9HGKW-2Bns3vo2w47bXjMti0bIse2ZGcbD8OPEnh3DvsXj2QgtWJ9QQdDiM81wYV5kLGdo9Opxt5O0QHk96DyNHRv-2FB9DRXFxYAMSr7Q6pmjijK6zyGPUwc5ckZTqRlL5k69o0lm7D2DcZaC9B5hQF0BsvorCWLkiiwKmTXrXz-2BAKy0mOGxQ5oHKE0oI-2FdTPQVb7CXH5WC7Xn95ddMHBw-3D
-
-var MySql = {
+﻿var MySql = {
     _internalCallback : function() { console.log("Callback not set")},
     Execute: function (Host, Username, Password, Database, Sql, Callback) {
         MySql._internalCallback = Callback;
