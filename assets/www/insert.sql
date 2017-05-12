1ca488fdf25d5dd533e26d5d74ac900fd1fb82de
Reshad Dernjani <a1409161@univie.ac.at>
2017-05-13 00:31:43 +0200

    Statistik computations are now final

--- assets/www/insert.sql
+++ assets/www/insert.sql
@@ -2,10 +2,11 @@ insert into users values(1, 'John','Wayne', 'password', 'admin@mail.com', true, 
 insert into users values(2, 'Max','Muster', 'password', 'max@mail.com', true, 'Ich bin Informatiker','06741233211', 'imglink', 'erste2', 'erste');
 insert into users values(3, 'Kein','ErsteKunde', 'password', 'kein@mail.com', false, null, '06741233211', 'imglink', null, 'bawag');
 
-
-insert into ads values(3, 1,'Traumhaus3 Finanzieren', 500000, 0, 4.5, 'Ich möchte ein Haus bauen!', 120, '2017-04-30', 0, null, null);
-insert into ads values(2, 1,'Traumhaus2 Finanzieren', 300000, 0, 4.5, 'Ich möchte ein Haus bauen!', 120, '2017-01-10', 1, 2, '2017-05-10');
-insert into ads values(1, 1,'Traumhaus1 Finanzieren', 600000, 0, 4.5, 'Ich möchte ein Haus bauen!', 120, '2001-05-10', 2, 2, '2002-06-10');
+insert into ads values(null, 1,'Traumhaus3 Finanzieren', 500000, 0, 1.5, 'Ich möchte ein Haus bauen!', 120, '2017-04-30', 0, null, null);
+insert into ads values(null, 1,'Traumhaus2 Finanzieren', 300000, 0, 1.5, 'Ich möchte ein Haus bauen!', 120, '2017-01-10', 1, 2, '2017-05-10');
+insert into ads values(null, 1,'Traumhaus1 Finanzieren', 600000, 0, 1.5, 'Ich möchte ein Haus bauen!', 120, '2001-05-10', 2, 2, '2002-06-10');
+insert into ads values(null, 2,'Auto Finanzieren', 1000, 0, 1.5, 'Ich möchte ein Auto kaufen!', 12, '2017-01-01', 1, 1, '2017-01-10');
+insert into ads values(null, 2,'TV Finanzieren', 500, 0, 1.5, 'Ich möchte ein TV kaufen!', 12, '2017-01-01', 1, 3, '2017-02-10');
 
 insert into favs values(1, 2, 3);
 insert into favs values(2, 2, 2);
